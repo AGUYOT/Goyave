5add6bad0cf428883e35f06f7ebd445888e6680f
alexguillon <cyclopedeveloppeur@gmail.com>
2019-09-30 16:17:34 +0100

    Routing feature implemented.

--- Services/Router/Routing.php
+++ Services/Router/Routing.php
@@ -4,19 +4,43 @@ namespace Services\Router;
 
 use Controllers\BaseController;
 
+  /**
+   * Routing
+   * Call the good controller according to the path given by the user.
+   * 
+   * @package    Router
+   * @author     alexguillon
+   */
 class Routing
 {
+    /**
+       * 
+       * Get the path contacted and call the controller adapted. 
+       * If the path does not exist, we call the error controller.
+       *
+       * @return integer
+       */
     function handle()
     {
- 
         $json = file_get_contents('../Services/Router/routes.json');
-        $link = substr($_SERVER['REQUEST_URI'], 25); 
+        $link = "/" . substr($_SERVER['REQUEST_URI'], 25); 
         $json_data = json_decode($json,true);
+        $routeFound = 0;
         foreach ($json_data["routes"] as $route) {
-            $actionArray = explode(":",$route["action"]);
-            $controller = $actionArray[0];
-            $action = $actionArray[1];
-            echo $controller . " " . $action . "\n";
+            if($route["path"] == $link){
+                $routeFound = 1;
+                $actionArray = explode(":",$route["action"]);
+                $action = $actionArray[1];
+                $controllerName = "\Controllers\\".$actionArray[0];
+                $controller = new $controllerName();
+                $controller->$action();
+            }
+        }
+        if($routeFound == 0){
+            $controllerName = "\Controllers\\ErrorController";
+            $controller = new $controllerName();
+            $controller->index();
         }   
+        return $routeFound;
     }
 }
